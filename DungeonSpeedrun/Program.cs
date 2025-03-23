92fa4e99053821b2ca2e2b6f2b6a6f1b901da400
FredTheKing <electromaster469@gmail.com>
2025-03-23 12:10:49 +0300

    init

---
+++ DungeonSpeedrun/Program.cs
@@ -0,0 +1,25 @@
+﻿using ImGuiNET;
+using rlImGui_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+
+
+InitWindow(1280, 720, "Hello, Dungeon!");
+rlImGui.Setup();
+
+while (!WindowShouldClose())
+{
+  BeginDrawing();
+  rlImGui.Begin();
+  ClearBackground(RayWhite);
+
+  ImGui.Begin("halo");
+  ImGui.Text("Hello, World!");
+  ImGui.End();
+  
+  rlImGui.End();
+  EndDrawing();
+}
+
+rlImGui.Shutdown();
+CloseWindow();
